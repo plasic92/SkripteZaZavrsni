84506144e5ed5ce8b88ec0320939aa51c3bc6427
plasic92 <plasic@algebra.hr>
2025-08-06 17:17:58 +0200

    Update TriggerOnFolderCreate.ps1

--- TriggerOnFolderCreate.ps1
+++ TriggerOnFolderCreate.ps1
@@ -1,4 +1,4 @@
-﻿param([int]$RecordID)
+param([int]$RecordID)
 
 function Get-ISO8601DateString  {
     param(
@@ -47,6 +47,44 @@ function Get-customEvents {
 }
 
 
+function Write-customEvents {
+    param(
+        $event11
+    )
+#flat list
+#$event11 = $lista|ForEach-Object {$_}
+
+#$event11[0].Properties[5].Value
+
+$customEventObject = foreach($event in $event11){
+    $TimeCreated = $event.TimeCreated.ToString("d.M.yyyy HH:mm:s.fff")
+    $TimeReceived = [datetime]::Now.ToString("d.M.yyyy HH:mm:s.fff")
+    #cijela putanja foldera isčitana sa eventloga
+    $ObjectName = $event.Properties[5].Value
+    if([string]::IsNullOrEmpty(([System.IO.FileInfo]$ObjectName).Extension)){
+        try{
+            #ne pravim custom object nego uzimam gotov objekt od access controlla i dodajem odgovarajuće props 
+            #get-acl je dosata sporiji od GetAccessControl() metode
+            $object = ([System.IO.FileInfo]$ObjectName).GetAccessControl().Access
+            $object|ForEach-Object{ Add-Member -InputObject $_ -MemberType NoteProperty -Name 'FolderPath' -Value $ObjectName}
+            $object|ForEach-Object{ Add-Member -InputObject $_ -MemberType NoteProperty -Name 'TimeCreated' -Value $TimeCreated}
+            $object|ForEach-Object{ Add-Member -InputObject $_ -MemberType NoteProperty -Name 'TimeReceived' -Value $TimeReceived}
+            $object
+        }catch{}
+    }
+}
+
+$putanja = 'C:\Users\Administrator.DOMENAZAPIS\Desktop\skripte\FolderCreate.csv'
+$header = '"FolderPath"*"TimeCreated"*"TimeReceived"*"FileSystemRights"*"AccessControlType"*"IdentityReference"*"IsInherited"*"ObjectName"*"InheritanceFlags"*"PropagationFlags"'
+
+$data = $customEventObject|ConvertTo-csv  -Delimiter *
+if(Test-Path -Path $putanja){
+   $data[2..($data.Count -1)]|Out-File -Encoding utf8 -FilePath $putanja -Append
+}else{
+    $data|Out-File -Encoding utf8 -FilePath $putanja
+}
+
+}
 
 #$RecordID = 19410
 
@@ -68,51 +106,22 @@ $log = Get-WinEvent -LogName "Microsoft-Windows-Sysmon/Operational" -FilterXPath
 #$mapa = [System.IO.FileInfo]$log.Properties[5].Value
 #if(-not [string]::IsNullOrEmpty($mapa.Extension)){exit}
 
-$lista = [System.Collections.Generic.List[System.Diagnostics.Eventing.Reader.EventLogRecord]]::new()
+#$lista = [System.Collections.Generic.List[System.Diagnostics.Eventing.Reader.EventLogRecord]]::new()
 
 $events = $log
-$lista.Add($events)
+#$lista.Add($events)
 
 
 #$ObjectName = $log.Properties[5].value
 while ($events){
-    sleep 3
+    Write-customEvents -event11 $events
     $bookmark = $events[-1].Bookmark
-    $events = Get-customEvents -logname "Microsoft-Windows-Sysmon/Operational" -XMLfilter $filter -reversed $false -bookmark $bookmark
+    $events = Get-customEvents -logname "Security" -XMLfilter $filter -reversed $false -bookmark $bookmark
     if($events){
-        $events.ForEach({$lista.Add($_)})
+        sleep 3
     }
 }
 
-#flat list
-$event11 = $lista|ForEach-Object {$_}
-
-#$event11[0].Properties[5].Value
-
-$customEventObject = foreach($event in $event11){
-    $TimeCreated = $event.TimeCreated.ToString("d.M.yyyy HH:mm:s.fff")
-    $TimeReceived = [datetime]::Now.ToString("d.M.yyyy HH:mm:s.fff")
-    $ObjectName = $event.Properties[5].Value
-    if([string]::IsNullOrEmpty(([System.IO.FileInfo]$ObjectName).Extension)){
-        try{
-            $object = ([System.IO.FileInfo]$ObjectName).GetAccessControl().Access
-            $object|ForEach-Object{ Add-Member -InputObject $_ -MemberType NoteProperty -Name 'FolderPath' -Value $ObjectName}
-            $object|ForEach-Object{ Add-Member -InputObject $_ -MemberType NoteProperty -Name 'TimeCreated' -Value $TimeCreated}
-            $object|ForEach-Object{ Add-Member -InputObject $_ -MemberType NoteProperty -Name 'TimeReceived' -Value $TimeReceived}
-            $object
-        }catch{}
-    }
-}
-
-$putanja = 'C:\Users\Administrator.DOMENAZAPIS\Desktop\skripte\FolderCreate.csv'
-$header = '"FolderPath"*"TimeCreated"*"TimeReceived"*"FileSystemRights"*"AccessControlType"*"IdentityReference"*"IsInherited"*"ObjectName"*"InheritanceFlags"*"PropagationFlags"'
-
-$data = $customEventObject|ConvertTo-csv  -Delimiter *
-if(Test-Path -Path $putanja){
-   $data[2..($data.Count -1)]|Out-File -Encoding utf8 -FilePath $putanja -Append
-}else{
-    $data|Out-File -Encoding utf8 -FilePath $putanja
-}
 
 
 
